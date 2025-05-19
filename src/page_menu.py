793c98f7411344ef30134962620e8dc190adf5b9
sangjin0214 <35863587+sangjin0214@users.noreply.github.com>
2025-05-20 07:58:03 +0900

    Update page_menu.py

--- src/page_menu.py
+++ src/page_menu.py
@@ -2,21 +2,7 @@ import gspread
 
 
 def page_menu(table_num):
-  template = '''
-  <html>
-    <head>
-      <title>메뉴 선택 페이지</title>
-    </head>
-    <body>
-      <p><h2 class="main-title">'''+table_num+'''번 테이블 주문</h2></p><br><br><br>
-      <form action="/payment" method="post">
-        '''+table_num+'''<br>
-        <input tpye="hidden" name="table_num" value="'''+table_num+'''">
-        <input type="submit" value="결제">
-      </form>
-    </body>
-  </html>'''
-  return template
-
-
   
+  return str(ws_menu.get('A1:G1'))
+
+
