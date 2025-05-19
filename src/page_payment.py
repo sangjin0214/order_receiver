6074980bffee74d4761b3102644491c6816532e9
sangjin0214 <35863587+sangjin0214@users.noreply.github.com>
2025-05-20 04:06:35 +0900

    Update page_payment.py

--- src/page_payment.py
+++ src/page_payment.py
@@ -16,9 +16,10 @@ def page_payment(order_state, table_num):
   template = '''
   <html>
     <head>
-      <p>'''+table_num+'''번 테이블 주문 사항</p><br><a href="/'''+table_num+'''">메뉴 선택 페이지로 돌아가기</a><br><br><br>
+      <title>결제 페이지</title>
     </head>
     <body>
+      <p><h2 class="main-title">'''+table_num+'''번 테이블 주문 사항</h2></p><br><a href="/'''+table_num+'''">메뉴 선택 페이지로 돌아가기</a><br><br><br>
       다음 주문 내역을 확인하고 [우리은행 1002-859-834337 (예금주 엄상진)]로 주문 금액을 입금한 후, 입금자명을 입력하고 완료를 눌러주세요.<br><br>
       <form action="/payment/order_complete" method="post">
         '''+template_bill(order_state)+'''<br>
