c1802e3af2effbfe2e7b1cf08684ad7c45899625
sangjin0214 <35863587+sangjin0214@users.noreply.github.com>
2025-05-20 02:44:56 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,24 +1,36 @@
 from flask import Flask, request, render_template
-from src import page_menu
+from src import page_menu, page_payment, input_order
+import os
+import json
+from google.oauth2.service_account import Credentials
+import gspread
 
 application = Flask(__name__)
+
+key_json_string = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+credentials_info = json.loads(key_json_string)
+credentials = Credentials.from_service_account_info(credentials_info)
+client = gspread.authorize(credentials)
+spreadsheet = client.open_by_key('1871ZjkgBWblqwsxkTxWSDqGy73M18Z27Qx9LPM_AIF0')
+ws_menu = spreadsheet('menu_available')
+
+
+@application.route("/payment", method=['POST'])
+def page_payment():
+  order_state = [request.form[menu] for menu in ws_menu.get('A1:G1')+ws_menu.get('A4:H4')]
+  table_num = request.form['table_num']
+  return page_payment.page_payment(order_state, table_num)
+
+
+@application.route("/payment/order_complete", method=['POST'])
+def page_complete():
+  order_state = [request.form[menu] for menu in ws_menu.get('A1:G1')+ws_menu.get('A4:H4')]
+  table_num = request.form['table_num']
+  orderer_name = request.form['orderer_name']
+  input_order.input_order(table_num, orderer_name, order_state)
+  return render_template('./src/page_complete.html', table=table_num, name=orderer_name)
 
 
 @application.route("/<table_num>")
 def page_menu(table_num):
   return page_menu.page_menu(table_num)
-
-
-@application.route("/payment", method=['POST'])
-def page_payment():
-  menu_price = []
-  order_state = [request.form[''],request.form[''],request.form[''],request.form['']]
-  sum = 0
-  for p, s in menu_price, order_state:
-    sum += p*s
-  return render_template('./src/page_payment.html', table_number=table_num, price=sum, n=order_state[0], ... )
-
-
-@application.route("/payment/order_complete", method=['POST'])
-def page_complete():
-  return render_template('./src/page_complete.html', table_number=table_num, name=orderer_name)
