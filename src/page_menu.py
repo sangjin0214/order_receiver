64fa9ed28bddec0e9fd354fe768c037d52e664b9
sangjin0214 <35863587+sangjin0214@users.noreply.github.com>
2025-05-21 01:23:10 +0900

    Update page_menu.py

--- src/page_menu.py
+++ src/page_menu.py
@@ -19,6 +19,10 @@ def page_menu(table_num):
       <title>메뉴 선택 페이지</title>
     </head>
     <body>
+      <img src="{{url_for("static", filename="001.png")}}" alt="메뉴판1" width="100%"><br>
+      <img src="{{url_for("static", filename="002.png")}}" alt="메뉴판2" width="100%"><br>
+      <img src="{{url_for("static", filename="003.png")}}" alt="메뉴판3" width="100%"><br>
+      <img src="{{url_for("static", filename="004.png")}}" alt="메뉴판4" width="100%"><br><br><br>
       <p><h2 class="main-title">'''+table_num+'''번 테이블 주문</h2></p><br><br>
       <form action="/payment" method="post">
         '''+template_menu()+'''<br>
