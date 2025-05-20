4d8b88024c5b501c1a19721ac74b6200d92071fd
sangjin0214 <35863587+sangjin0214@users.noreply.github.com>
2025-05-20 09:20:06 +0900

    Update page_payment.py

--- src/page_payment.py
+++ src/page_payment.py
@@ -1,4 +1,17 @@
+import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
 import gspread
+
+SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
+
+key_json_string = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+credentials_info = json.loads(key_json_string)
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_info, SCOPES)
+client = gspread.authorize(credentials)
+spreadsheet = client.open_by_key('1871ZjkgBWblqwsxkTxWSDqGy73M18Z27Qx9LPM_AIF0')
+ws_menu = spreadsheet.worksheet('menu_available')
+ws_order = spreadsheet.worksheet('order_contents')
 
 
 def page_payment(order_state, table_num):
